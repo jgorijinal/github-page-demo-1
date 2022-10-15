13603a663fe56e78e533f90910a2d26d3877c925
jgoriginal <932274204@qq.com>
2022-10-15 14:32:48 +0800

    引入 vue-router

---
+++ src/router/index.ts
@@ -0,0 +1,14 @@
+import {createRouter, createWebHashHistory,RouteRecordRaw } from 'vue-router'
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    name: 'home',
+    component:() => import('../views/home.vue')
+  }
+]
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+})
+export default router
---
+++ src/views/home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    {{count}}
+  </div>
+  <button @click="onClick">+1</button>
+</template>
+<script setup lang="ts">
+import {ref} from 'vue'
+const count = ref<number>(0)
+const onClick = () =>{
+  count.value += 1
+}
+</script>
