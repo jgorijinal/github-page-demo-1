b9f29cf404f548c4d420c7eb42519ff64c847244
jgoriginal <932274204@qq.com>
2022-10-18 16:27:13 +0800

    封装 navBar 组件

--- src/components/navBar.vue
+++ src/components/navBar.vue
@@ -1,10 +1,95 @@
 <template>
-  <div>
-    navBar
+  <div class="eren-navBar-content">
+    <!--左侧区域-->
+    <div class="eren-navBar-left" @click="clickLeft">
+      <span class="eren-navBar-left-icon">
+        <slot name="leftIcon"></slot>
+      </span>
+      <span class="eren-navBar-left-text" v-if="leftText">
+        {{ leftText }}
+      </span>
+    </div>
+    <!--中间标题-->
+    <span class="eren-navBar-title" v-if="title">{{ title }}</span>
+    <!--右侧区域-->
+    <div class="eren-navBar-right" @click="clickRight">
+      <span class="eren-navBar-left-text" v-if="rightText">
+        {{ rightText }}
+      </span>
+      <span class="eren-navBar-right-icon">
+        <slot name="rightIcon"></slot>
+      </span>
+    </div>
   </div>
 </template>
 <script lang="ts" setup>
-
+export interface NavBarProps {
+  leftText?: string
+  title?: string
+  rightText?:string
+}
+const props = defineProps<NavBarProps>()
+const emits = defineEmits(['clickLeft','clickRight'])
+// 点击左侧区域
+const clickLeft = () => {
+  emits('clickLeft')
+}
+// 点击右侧区域
+const clickRight = () => {
+  emits('clickRight')
+}
 </script>
 <style lang="scss" scoped>
+.eren-navBar-content {
+  width:100%;
+  background-image: linear-gradient(to bottom, #002ea6, #3667e3);
+  color:white;
+  position:relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height:46px;
+  .eren-navBar-left{
+    position:absolute;
+    // 使用 绝对定位 撑满区域(下面三行), 并加上 左右padding 增加区域宽度
+    left: 0;
+    top: 0;
+    bottom: 0;
+    padding:0 16px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    &-icon {
+      margin-right:6px;
+      display: flex;
+      align-items: center;
+    }
+    &-text {
+      
+    }
+    &:hover,&:active {
+      background-color:#3667e3;
+    }
+  }
+  .eren-navBar-title{
+    
+  }
+  .eren-navBar-right{
+    position:absolute;
+    // 使用 绝对定位 撑满区域(下面三行), 并加上 左右padding 增加区域宽度
+    right:0;
+    top: 0;
+    bottom: 0;
+    padding:0 16px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    &-icon {
+      margin-left:6px;
+    }
+    &-text {
+
+    }
+  }
+}
 </style>
--- src/views/start.vue
+++ src/views/start.vue
@@ -1,17 +1,35 @@
 <template>
+    <nav-bar left-text="日常记账"
+    @click-left="clickLeftHandle" @click-right="clickRightHandle">
+      <template #leftIcon>
+        <img :src="menuSvg" alt="" class="left-icon-img">
+      </template>
+    </nav-bar>
     <e-button class="button" @click="handleClick">开始记账</e-button>
     <float-button />
 </template>
 <script setup lang="ts">
 import EButton from '../components/button.vue'
 import FloatButton from '../components/floatButton.vue'
+import menuSvg from '../assets/icons/menu.svg'
+import navBar from '../components/navBar.vue'
 const handleClick = () => {
   console.log('hi')
+}
+const clickLeftHandle = () => {
+  console.log('点击了 navBar的左侧区域')
+}
+const clickRightHandle = () => {
+  console.log('点击了 navBar的右侧区域')
 }
 </script>
 <style lang="scss" scoped>
 .button {
   display: block;
   margin:10px;
+}
+.left-icon-img {
+  width:1.2em;
+  height:1.2em;
 }
 </style>
