168100155a74579baf6efe3b3448ccbdbd724e6c
jgoriginal <932274204@qq.com>
2022-10-22 15:03:32 +0800

    D:/Software/cmder/vendor/git-for-windows/item/list 页面使用 tabs 组件

--- src/views/item/itemList.vue
+++ src/views/item/itemList.vue
@@ -5,16 +5,26 @@
     </template>
   </nav-bar>
   <overlay-with-route :overlayVisible="overlayVisible" @close-overlay="overlayVisible = false"/>
+  <!--标签页-->
+  <tabs v-model:active="activeName">
+    <tab name="本月"></tab>
+    <tab name="上月"></tab>
+    <tab name="今年"></tab>
+    <tab name="自定义时间"></tab>
+  </tabs> 
 </template>
 <script setup lang="ts">
 import navBar from '../../components/navBar.vue'
-// import tabs from '../../components/tabs.vue'
-// import tab from '../../components/tab.vue'
+import tabs from '../../components/tabs.vue'
+import tab from '../../components/tab.vue'
 import menuSvg from '../../assets/icons/menu.svg'
 
 import overlayWithRoute from '../../components/overlayWithRoute.vue'
 import { ref } from 'vue'
 const overlayVisible = ref(false)
+
+
+const activeName = ref('本月')
 </script>
 <style lang="scss" scoped>
 .left-icon-img {
