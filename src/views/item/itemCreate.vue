0334846c501b52449638319f6a795249c4679242
jgoriginal <932274204@qq.com>
2022-10-22 12:27:15 +0800

    创建标签列表

--- src/views/item/itemCreate.vue
+++ src/views/item/itemCreate.vue
@@ -8,7 +8,8 @@
     </nav-bar>
     <!--标签栏-->
     <tabs v-model:active="activeName">
-      <tab name="支出">内容 1</tab>
+      <tab name="支出">
+      </tab>
       <tab name="收入">内容 2</tab>
     </tabs>
     <!--数字键盘-->
@@ -38,6 +39,18 @@ const activeName = ref('支出')
 const submitHandle = (amount:string) => {
   console.log(amount)
 }
+// 标签列表 - 支出
+const expensesTags = ref([
+  { id: 1, tagName: '餐费', emoji: 'xxx' },
+  { id: 2, tagName: '打车费', emoji: 'yyy' },
+  {id:3, tagName:'聚餐',emoji:'zzz'}
+])
+// 标签列表 - 收入
+const inComeTags = ref([
+  { id: 4, tagName: '工资', emoji: 'vvv' },
+  { id:5, tagName: '彩票', emoji: 'bbb' },
+  {id:6, tagName: '抢劫',emoji:'jjj'}
+])
 </script>
 <style lang="scss" scoped>
 img {
