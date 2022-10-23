e0c473570d4f1a8bd157dd5f196addd7e7ef1757
jgoriginal <932274204@qq.com>
2022-10-23 14:22:17 +0800

    优化 select 控件样式

--- src/views/statistics/components/charts.vue
+++ src/views/statistics/components/charts.vue
@@ -1,16 +1,20 @@
 <template>
   <div class="eren-chart-container">
+    <div class="eren-chart-select">
+      <span>类型</span>
+      <!--select 控件-->
     <select :value="modelValue" @change="selectChange">
       <option disabled value="">请选择</option>
       <option value="expense">支出</option>
       <option value="income">收入</option>
     </select>
+    </div>
+    <!--图表-->
   </div>
-  {{modelValue}}
 </template>
 <script setup lang="ts">
 import { ref} from 'vue'
-// 开始时间, 结束时间
+// 开始时间, 结束时间, 支出/收入类型 v-model 绑定
 interface chartsProps {
   startDate: string
   endDate: string
@@ -21,17 +25,22 @@ const emits = defineEmits(['update:modelValue'])
 
 // 支出,收入切换动作
 const selectChange = (e:any) => {
-  console.log(e.target.value) // 支出,收入
+  // console.log(e.target.value) // 支出,收入
   emits('update:modelValue',e.target.value)
 }
 </script>
 <style lang="scss" scoped>
 .eren-chart-container{
   padding:8px;
-  select{
+  .eren-chart-select{
+    display: flex;
+    align-items: center;
+    select{
+      margin-left:10px; 
     width:80px;
     height:30px;
     border:1px solid #3465e0;
+  }
   }
 }
 </style>
