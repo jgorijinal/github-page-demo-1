6b0f97c2fac9c4ea306d1024234b286691f72b95
jgoriginal <932274204@qq.com>
2022-10-25 14:34:33 +0800

    实现选中标签

--- src/views/item/itemCreate.vue
+++ src/views/item/itemCreate.vue
@@ -16,7 +16,10 @@
               新增标签
             </span>
           </div>
-          <div class="eren-tag-container" v-for="tag in expensesTags" :key="tag.id" @click="$router.push(`/tag/${tag.id}/edit?kind=expenses`)">
+          <div class="eren-tag-container" 
+          v-for="tag,index in expensesTags" :key="tag.id" 
+          @click="onSelect(tag)" 
+          :class="{selected:tag_ids.includes(tag.id)}">
             <span class="eren-tag-emoji">
               {{tag.sign}}
             </span>
@@ -34,7 +37,7 @@
             新增标签
           </span>
         </div>
-        <div class="eren-tag-container" v-for="tag in incomeTags" :key="tag.id" @click="$router.push(`/tag/${tag.id}/edit?kind=income`)">
+        <div class="eren-tag-container" v-for="tag in incomeTags" :key="tag.id" @click="onSelect">
           <span class="eren-tag-emoji">
             {{tag.sign}}
           </span>
@@ -52,6 +55,8 @@
   </div>
 </template>
 <script lang="ts" setup>
+// $router.push(`/tag/${tag.id}/edit?kind=expenses`)
+// $router.push(`/tag/${tag.id}/edit?kind=income`)
 // 组件
 import navBar from '../../components/navBar.vue'
 import tabs from '../../components/tabs.vue'
@@ -61,7 +66,7 @@ import inputPad from '../../components/inputPad.vue'
 import fanhuiSvg from '../../assets/icons/fanhui.svg'
 import add2Svg from '../../assets/icons/add2.svg'
 // vue
-import { ref } from 'vue'
+import { ref,computed } from 'vue'
 import { useRouter } from 'vue-router'
 
 import { getTags } from '../../api/tags'
@@ -70,10 +75,6 @@ const leftClickHandle = () => {
   router.back()
 }
 const activeName = ref('支出')
-// 监听键盘提交事件
-const submitHandle = (amount:string) => {
-  console.log(amount)
-}
 const expensesTags = ref<any>([])
 const incomeTags = ref<any>([])
 const page = ref(0) // 当前页码
@@ -88,9 +89,26 @@ const getIncomeTags = async() => {
   const { resources,pager } = await getTags({ page: (page.value + 1), kind: 'income' }) 
   incomeTags.value = resources
 }
-
 getExpenseTags()
 getIncomeTags()
+
+// 选中的标签- 数组
+const tag_ids = ref<Number[]>([])
+// 选中标签
+const onSelect = (tag:any) => {
+  const isIncludes = tag_ids.value.includes(tag.id)
+  if (!isIncludes) {
+    tag_ids.value.push(tag.id) 
+  } else {
+    tag_ids.value = tag_ids.value.filter(id => id !== tag.id)
+  }
+
+  console.log(tag_ids.value)
+}
+// 监听键盘提交事件
+const submitHandle = (amount:string) => {
+  console.log(amount)
+}
 </script>
 <style lang="scss" scoped>
 img {
@@ -126,17 +144,18 @@ img {
       justify-content: flex-start;
       background-color:transparent;
     }
-    &:hover,&:active {
-      background-color: #fff6d6;
+    &.selected {
+      background-color: #fde590;
     }
   }
   &-emoji{
     border-radius:50%;
     width:14vw;
     height:14vw;
-    background-color: rgb(235, 235, 235);
+    background-color: #edecec;
     display: flex;
     justify-content: center;
+    border:1px solid #3465e0;
     align-items: center;
     &.add {
       background-color: white;
