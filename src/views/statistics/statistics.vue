0f038965fde3a1df3c7d7cad64b294b0907e5eac
jgoriginal <932274204@qq.com>
2022-10-26 17:40:00 +0800

    标签页的类型互相不影响

--- src/views/statistics/statistics.vue
+++ src/views/statistics/statistics.vue
@@ -7,18 +7,18 @@
   <div class="container-wrapper">
     <tabs v-model:active="activeName">
     <tab name="本月">
-      <charts  :startDate="getThisMonth().startDate" :endDate="getThisMonth().endDate" v-model="activeSelect"/>
+      <charts  :startDate="getThisMonth().startDate" :endDate="getThisMonth().endDate" v-model="activeSelect1"/>
     </tab>
     <tab name="上月">
-      <charts  :startDate="getLastMonth().startDate" :endDate="getLastMonth().endDate" v-model="activeSelect"/>
+      <charts  :startDate="getLastMonth().startDate" :endDate="getLastMonth().endDate" v-model="activeSelect2"/>
     </tab>
     <tab name="今年">
-      <charts  :startDate="getThisYear().startDate" :endDate="getThisYear().endDate" v-model="activeSelect"/>
+      <charts  :startDate="getThisYear().startDate" :endDate="getThisYear().endDate" v-model="activeSelect3"/>
     </tab>
     <tab name="自定义时间">
           <!--日期选择器(二次封装)-->
         <date-picker @date-changed="dateChanged"/>
-        <charts  :startDate="customDate.startDate" :endDate="customDate.endDate" v-model="activeSelect"/>
+        <charts  :startDate="customDate.startDate" :endDate="customDate.endDate" v-model="activeSelect4"/>
     </tab>
   </tabs>
   </div>
@@ -51,7 +51,10 @@ const dateChanged = (date:any) => {
 }
 
 // 类型选择 收入income / 支出 expenses
-const activeSelect = ref('expenses')
+const activeSelect1 = ref('expenses')
+const activeSelect2 = ref('expenses')
+const activeSelect3 = ref('expenses')
+const activeSelect4 = ref('expenses')
 </script>
 <style lang="scss" scoped>
 img{
